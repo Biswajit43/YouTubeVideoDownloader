66b045e28198d3b1a278127b71cba442aabfc1e8
Biswajit43 <143047694+Biswajit43@users.noreply.github.com>
2025-09-11 15:42:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,6 @@ app.use(cors({
 
 // Handle preflight requests for all routes
 // Handle preflight requests for all routes
-app.options("/*", cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true
-}));
-
 
 
 const server = http.createServer(app);
